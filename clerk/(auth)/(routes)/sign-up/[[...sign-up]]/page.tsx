bf0e2b67999961651d9713e0ad501527facb17cd
sujal <sujalkesharwani220@gmail.com>
2025-03-24 01:01:13 +0530

    some fixes

--- clerk/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ clerk/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="text-2xl bg-red-500 px-4 py-2 rounded">
         Welcome! at Sign Up page {user.fullName}
       </div>
-      <p>Your email is {user.primaryEmailAddress?.emailAddress}</p>
+      {/* <p>Your email is {user.primaryEmailAddress?.emailAddress}</p> */}
     </div>
   );
 }
--- clerk/(auth)/layout.tsx
+++ clerk/(auth)/layout.tsx
@@ -1,8 +1,12 @@
-
+import { ClerkProvider } from "@clerk/nextjs";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-yellow-700 to-orange-300">{children}</div>
+    <ClerkProvider>
+      <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-yellow-700 to-orange-300">
+        {children}
+      </div>
+    </ClerkProvider>
   );
 };
 
